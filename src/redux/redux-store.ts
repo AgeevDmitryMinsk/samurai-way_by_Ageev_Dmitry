13ce272d160b89ca0683c605f313083912769d11
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-05-08 20:24:04 +0300

    add redux-store at main-branch

---
+++ src/redux/redux-store.ts
@@ -0,0 +1,13 @@
+import {combineReducers, createStore} from "redux";
+import {messagesReducer} from "./messages-reducer";
+import {profileReducer} from "./profile-reducer";
+
+const rootReducer = combineReducers({
+	ProfilePage: profileReducer, // за свойство ProfilePage отвечает profileReducer
+	DialogsPage: messagesReducer // по аналогии
+})
+
+export const store = createStore(rootReducer)
+
+// определить автоматически тип всего объекта состояния
+export type AppRootStateType = ReturnType<typeof rootReducer>
