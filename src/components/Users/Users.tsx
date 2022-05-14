4bd02039e31bce6e243230b017ac6bff858b287d
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-05-14 19:00:41 +0300

    Lesson_53: by Dimich: convert FunctionComponent Users to  classComponent Users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -11,8 +11,6 @@ import {UsersResponseType} from "../../redux/users-reducer";
 // import {ThunkAction, ThunkDispatch} from "redux-thunk";
 // // API KEY: 57f858ff-ce33-4672-b278-3f2f1b802b55
 // //Ваш ID: 22100
-
-
 // type ThunkType = ThunkAction<any, any, any, any>
 // type ThunkDispatch1 = ThunkDispatch<any, any, any>
 // const instance = axios.create({
@@ -37,21 +35,19 @@ import {UsersResponseType} from "../../redux/users-reducer";
 // }
 
 
-export const Users = (props: UsersPropsType) => {
+export class Users extends React.Component<UsersPropsType> {
 
-	// для избежания сайд-эффекта при первой загрузке компоненты делаем костыль-заглушку, т.е.
-	// оборачиваем код в функцию const getUsers(), затем в рендрере делаем копку GET USERS для вызова костыля
-	const getUsers = () => {
-		if (props.users.length === 0) {
-			return axios.get("https://social-network.samuraijs.com/api/1.0/users"
+	constructor(props: any) {
+		 super(props);
+			 axios.get("https://social-network.samuraijs.com/api/1.0/users"
 				//, {	baseURL: "https://social-network.samuraijs.com/api/1.0",
 				// withCredentials: true,
 				// headers: {"API-KEY" : "57f858ff-ce33-4672-b278-3f2f1b802b55"}}
-				 ).then((response: AxiosResponse<UsersResponseType>) => {
-					 //debugger
-					console.log(response.data)
-					props.setUsers(response.data.items)
-				})
+			).then((response: AxiosResponse<UsersResponseType>) => {
+				//debugger
+				console.log(response.data)
+				this.props.setUsers(response.data.items)
+			})
 			// props.setUsers([
 			// 	{
 			// 		id: '1',
@@ -86,30 +82,81 @@ export const Users = (props: UsersPropsType) => {
 			// 		location: {city: "Minsk", country: "Belarus"}
 			// 	}
 			// ] )
-		}
+
 	}
+	render() {
 
-	return (
-		<div>
-			<button onClick={getUsers}>GET USERS</button>
-			{props.users.map(el => (
-				<div key={el.id} className={styles.container}>
-					<div className={styles.item}>
-						<div>
-							{/*<img src={el.photo} alt="usersAvatar"/>*/}
-							<img src={el.photos.large || Dima_photo} alt="usersAvatar"/>
-						</div>
-						{el.followed ?
+		// для избежания сайд-эффекта при первой загрузке компоненты делаем костыль-заглушку, т.е.
+		// оборачиваем код в функцию const getUsers(), затем в рендрере делаем копку GET USERS для вызова костыля
+		// const getUsers = () => {
+		// 	// if (this.props.users.length === 0) {
+		// 	// 	return axios.get("https://social-network.samuraijs.com/api/1.0/users"
+		// 	// 		//, {	baseURL: "https://social-network.samuraijs.com/api/1.0",
+		// 	// 		// withCredentials: true,
+		// 	// 		// headers: {"API-KEY" : "57f858ff-ce33-4672-b278-3f2f1b802b55"}}
+		// 	// 	).then((response: AxiosResponse<UsersResponseType>) => {
+		// 	// 		//debugger
+		// 	// 		console.log(response.data)
+		// 	// 		this.props.setUsers(response.data.items)
+		// 	// 	})
+		// 	// 	// props.setUsers([
+		// 	// 	// 	{
+		// 	// 	// 		id: '1',
+		// 	// 	// 		photo: Dima_photo,
+		// 	// 	// 		followed: false,
+		// 	// 	// 		fullName: "Dmitry A",
+		// 	// 	// 		status: `I'm a boss`,
+		// 	// 	// 		location: {city: "Minsk", country: "Belarus"}
+		// 	// 	// 	},
+		// 	// 	// 	{
+		// 	// 	// 		id: '2',
+		// 	// 	// 		photo: Natasha_photo,
+		// 	// 	// 		followed: true,
+		// 	// 	// 		fullName: "Natalia A",
+		// 	// 	// 		status: `I'm a house keeper`,
+		// 	// 	// 		location: {city: "Minsk", country: "Belarus"}
+		// 	// 	// 	},
+		// 	// 	// 	{
+		// 	// 	// 		id: '3',
+		// 	// 	// 		photo: Ksenia_photo,
+		// 	// 	// 		followed: true,
+		// 	// 	// 		fullName: "Ksenia A",
+		// 	// 	// 		status: `I'm a student`,
+		// 	// 	// 		location: {city: "Minsk", country: "Belarus"}
+		// 	// 	// 	},
+		// 	// 	// 	{
+		// 	// 	// 		id: '4',
+		// 	// 	// 		photo: Vera_photo,
+		// 	// 	// 		followed: true,
+		// 	// 	// 		fullName: "Vera A",
+		// 	// 	// 		status: `I'm a student`,
+		// 	// 	// 		location: {city: "Minsk", country: "Belarus"}
+		// 	// 	// 	}
+		// 	// 	// ] )
+		// 	// }
+		// }
+debugger
+		return (
+			<div>
+				{/*<button onClick={getUsers}>GET USERS</button>*/}
+				{this.props.users.map(el => (
+					<div key={el.id} className={styles.container}>
+						<div className={styles.item}>
 							<div>
-								<button onClick={() => props.unFollow(el.id)}>Follow</button>
+								{/*<img src={el.photo} alt="usersAvatar"/>*/}
+								<img src={el.photos.large || Dima_photo} alt="usersAvatar"/>
 							</div>
-							:
-							<div>
-								<button onClick={() => props.follow(el.id)}>Unfollow</button>
-							</div>}
-					</div>
+							{el.followed ?
+								<div>
+									<button onClick={() => this.props.unFollow(el.id)}>Follow</button>
+								</div>
+								:
+								<div>
+									<button onClick={() => this.props.follow(el.id)}>Unfollow</button>
+								</div>}
+						</div>
 
-					<span className={styles.description}>
+						<span className={styles.description}>
 						<div className={styles.item}>
 							<div style={{
 								fontWeight: "bold",
@@ -120,14 +167,15 @@ export const Users = (props: UsersPropsType) => {
 							<div>{el.status}</div>
 						</div>
 
-						{/*<div className={styles.item}>*/}
-						{/*	<div>{el.location.country}</div>*/}
-						{/*	<div>{el.location.city}</div>*/}
-						{/*</div>*/}
+							{/*<div className={styles.item}>*/}
+							{/*	<div>{el.location.country}</div>*/}
+							{/*	<div>{el.location.city}</div>*/}
+							{/*</div>*/}
 					</span>
-				</div>))}
-		</div>
-	);
-};
+					</div>))}
+			</div>
+		);
+	}
+}
 
 
