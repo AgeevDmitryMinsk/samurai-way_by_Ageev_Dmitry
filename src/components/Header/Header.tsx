e2af13a4ce15af69937d714ff0754b5798e12522
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-06-01 21:27:24 +0300

    Lesson_76: yarn add yup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -31,7 +31,7 @@ export const Header = (props: HeaderType) => {
 			<div className={s.login_block}>
 				{props.isAuth
 					? <>
-						{props.data.login}
+						login: {props.data.login}
 						<img src={props.profile.photos?.large || ``} alt="myPhoto"/>
 						{/*<img src={props.profile[`photos`][`large`] || `` } alt="myPhoto"/>*/}
 						<>Мой статус:</>
